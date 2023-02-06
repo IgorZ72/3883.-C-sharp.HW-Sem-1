6c7a2bf1a750d31188b6f36a8479cdd50360f495
igor <Igor.Zaika2010@yandex.ru>
2023-02-06 15:06:26 +0300

    первый комит

---
+++ Task 2.cs
@@ -0,0 +1,15 @@
+﻿// написать программу которая на вход принимает два числа 
+// и выдаёт большее число.
+Console.Clear();
+Console.Write("введите число a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("введите число b:");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("большее число a");
+}
+else 
+{
+    Console.Write("большее число b");
+}
