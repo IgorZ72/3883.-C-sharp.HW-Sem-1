e30cb30ccd0d092b7792a50351edd5dd97ec9db3
IgorZ72 <123865022+IgorZ72@users.noreply.github.com>
2023-02-06 15:38:38 +0300

    Add files via upload

---
+++ Task 4.cs
@@ -0,0 +1,32 @@
+﻿// написать программу, которая на вход принимает три числа 
+// и выдаёт максимальное.
+Console.Clear();
+Console.Write("введите число a:");
+int a = int.Parse(Console.ReadLine());
+Console.Write("введите число b:");
+int b = int.Parse(Console.ReadLine());
+Console.Write("введите число c:");
+int c = int.Parse(Console.ReadLine());
+if ((a > b) && (a > c))
+{
+    Console.Write("максимальное число a");
+}
+if ((b > a) && (b > c))
+{
+    Console.Write("максимальное число b");
+}
+if ((c > a) && (c > b))
+{
+    Console.Write("максимальное число c");
+}
+
+
+
+
+
+
+
+
+
+
+
