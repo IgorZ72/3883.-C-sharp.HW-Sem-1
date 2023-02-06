16ee7040034d9ecfc42f4147471cfcf86eb44eec
IgorZ72 <123865022+IgorZ72@users.noreply.github.com>
2023-02-06 16:23:27 +0300

    Add files via upload

---
+++ Task 8.cs
@@ -0,0 +1,36 @@
+﻿// написать программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("введите число n: ");
+int n = int.Parse(Console.ReadLine());
+int b =1;
+while (b < n)
+   { 
+   if (b % 2 == 0)
+    Console.WriteLine($"{b}"); 
+    b++;
+    //Console.WriteLine(b + ""); 
+    //b++;
+   }
+
+   
+
+
+
+
+
+
+
+
+
+
+  
+
+ 
+
+
+
+
+
+
+
