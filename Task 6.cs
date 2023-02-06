4c0e2dbd343b9c60ebf69677288600debf39684a
IgorZ72 <123865022+IgorZ72@users.noreply.github.com>
2023-02-06 16:19:56 +0300

    Add files via upload

---
+++ Task 6.cs
@@ -0,0 +1,14 @@
+﻿// написать программу,котоорая на вход принимает число, а
+// на выходе выдаёт является ли число чётным.
+Console.Clear();
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.Write("введённое число чётное");
+}
+else
+{
+    Console.Write("введённое число не чётное");
+}
+
